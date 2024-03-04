7ba01ad85a59d2504dd4f3d0e59cbc2f8910b3b0
vanjamihajlovic <mihajlovicavanja@gmail.com>
2024-03-04 11:04:35 +0100

    Added nav-bar and info component

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AdminComponent } from './admin/admin.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AdminComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
